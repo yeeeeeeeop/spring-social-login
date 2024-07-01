f24e3a7aac1f7dbd711e561b2cfe320ebe75f8ea
yeeeeeeeop <yeop0916@gmail.com>
2024-07-01 11:25:35 +0900

    init

---
+++ src/main/java/me/yeop/SocialLoginApplication.java
@@ -0,0 +1,11 @@
+package me.yeop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SocialLoginApplication.class, args);
+    }
+}
